1f44f5c0872339f44fad46c4c327e4d911a1d457
debjit-mpcl <debjit@mpcl.in>
2024-05-16 18:48:18 +0530

    Update mistral.py

--- 5_mins_rag_no_gpu/mistral.py
+++ 5_mins_rag_no_gpu/mistral.py
@@ -62,18 +62,19 @@ with st.sidebar:
     DOCS_DIR = os.path.abspath("./uploaded_docs")
     if not os.path.exists(DOCS_DIR):
         os.makedirs(DOCS_DIR)
-    '''
+    
     st.subheader("Add to the Knowledge Base")
     with st.form("my-form", clear_on_submit=True):
-        uploaded_files = st.file_uploader("Upload a file to the Knowledge Base:", accept_multiple_files = True)
-        submitted = st.form_submit_button("Upload!")
+        #uploaded_files = st.file_uploader("Upload a file to the Knowledge Base:", accept_multiple_files = True)
+        #submitted = st.form_submit_button("Upload!")
+        pass
 
     if uploaded_files and submitted:
         for uploaded_file in uploaded_files:
             st.success(f"File {uploaded_file.name} uploaded successfully!")
             with open(os.path.join(DOCS_DIR, uploaded_file.name),"wb") as f:
                 f.write(uploaded_file.read())
-    '''
+    
 
 user_input=None
 if 'common_ques' not in ss:
